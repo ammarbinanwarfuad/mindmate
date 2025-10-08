8f74ddc4f8c0115a967a3e77b19628c3f81c267a
ammarbin <ammarbinanwarfuad@gmail.com>
2025-10-08 14:31:48 +0600

    updated app/(auth)/register/page.tsx

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,14 @@
+import RegisterForm from '@/components/auth/RegisterForm';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
+
+export default async function RegisterPage() {
+  const session = await getServerSession(authOptions);
+  
+  if (session) {
+    redirect('/dashboard');
+  }
+
+  return <RegisterForm />;
+}
